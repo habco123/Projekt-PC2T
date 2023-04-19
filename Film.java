02e3918110b13bb2f3822614c945ede62b2244ac
habco123 <marekhabez1313@gmail.com>
2023-04-19 11:59:32 +0200

    added Film.java

---
+++ Film.java
@@ -0,0 +1,32 @@
+public class Film {
+    private String film_name;
+    private String director_name;
+    private int release_year;
+    private int review;
+    private String[] actors;
+
+    public Film(String film_name, String director_name, int release_year, int review, String[] actors){
+        this.film_name = film_name;
+        this.director_name = director_name;
+        this.release_year = release_year;
+        this.review = review;
+        this.actors = actors;
+    }
+
+    public String getFilm_name(){
+        return film_name;
+    }
+
+    public String getDirector_name(){
+        return director_name;
+    }
+    public int getRelease_year(){
+        return release_year;
+    }
+    public int getReview(){
+        return review;
+    }
+    public String[] getActors(){
+        return actors;
+    }
+}
