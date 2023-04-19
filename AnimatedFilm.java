4a77af3e6a17d673eb544a3461b825b027b15f62
habco123 <marekhabez1313@gmail.com>
2023-04-19 14:23:19 +0200

    pridanie abstract class Filmy a pridanie AnimatedFilm

---
+++ AnimatedFilm.java
@@ -0,0 +1,12 @@
+public class AnimatedFilm extends Filmy{
+    
+    private int recommended_age;
+
+    public AnimatedFilm(String film_name, String director_name, int release_year, int review, String[] actors, int recommended_age){
+        super(film_name, director_name, release_year, review, actors);
+        this.recommended_age = recommended_age;
+    }
+    public int getReccomendedAge(){
+        return recommended_age;
+    }
+}
