787b5a9dee32d8deb88b5d9107c214c400fc3ed6
habco123 <marekhabez1313@gmail.com>
2023-04-23 21:18:06 +0200

    pridanie io pre databazy pre animak aj normal

--- Databaza.java
+++ Databaza.java
@@ -1,9 +1,10 @@
 import java.sql.*;
-import java.util.Arrays;
 
 public class Databaza {
-    private String meno;
+    private String meno_normal;
+    private String meno_animated;
     private Connection conn;
+    
     public boolean connect(){
         try{
             conn = DriverManager.getConnection("jdbc:sqlite:Filmy.db");
@@ -24,7 +25,7 @@ public class Databaza {
         }
         return false;
     }
-    public boolean createTable(){
+    public boolean createTableNormal(){
         String sql = "CREATE TABLE IF NOT EXISTS NormalneFilmy (" + "id integer PRIMARY KEY, " + "Film_name TEXT NOT NULL," + "Director_name TEXT NOT NULL," + "release_year int NOT NULL," + "actors TEXT NOT NULL" + ");";
         try{
             Statement stmt = conn.createStatement();
@@ -35,7 +36,32 @@ public class Databaza {
         }
         return false;
     }
-    public void insertRecord(String meno, String director, int year, String actors){
+    public boolean createTableAnimated(){
+        String sql = "CREATE TABLE IF NOT EXISTS AnimovaneFilmy (" + "id integer PRIMARY KEY, " + "Film_name TEXT NOT NULL," + "Director_name TEXT NOT NULL," + "release_year int NOT NULL," + "actors TEXT NOT NULL," + "min_age int " +");";
+        try{
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+            return true;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public void insertRecordAnimated(String meno, String director, int year, String actors, int min_age){
+        String sql = "INSERT INTO AnimovaneFilmy(Film_name,Director_name,release_year,actors,min_age) VALUES (?,?,?,?,?)";
+        try{
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, meno);
+            pstmt.setString(2, director);
+            pstmt.setInt(3, year);
+            pstmt.setString(4, actors);
+            pstmt.setInt(5, min_age);
+            pstmt.execute();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void insertRecordNormal(String meno, String director, int year, String actors){
         String sql = "INSERT INTO NormalneFilmy(Film_name,Director_name,release_year,actors) VALUES (?,?,?,?)";
         try{
             PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -48,33 +74,55 @@ public class Databaza {
             e.printStackTrace();
         }
     }
-    public String getMeno(){
-        return meno;
+    public String getMenoNormal(){
+        return meno_normal;
     }
-    public Film getRecord(){
+    public String getMenoAnimated(){
+        return meno_animated;
+    }
+    public AnimatedFilm getRecordAnimated(){
+        AnimatedFilm film = null;
+        String sql = "SELECT id,Film_name,Director_name,release_year,actors,min_age FROM AnimovaneFilmy";
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                String film_name = rs.getString("film_name");
+                String herci = rs.getString("actors");
+                this.meno_animated = film_name;
+                StringBuilder builder = new StringBuilder(herci);
+                builder.deleteCharAt(herci.length() - 1);
+                builder = new StringBuilder(builder);
+                builder.deleteCharAt(0);
+                String[] actors = builder.toString().split(",");
+                film = new AnimatedFilm(film_name, rs.getString("Director_name"), rs.getInt("release_year"), actors, rs.getInt("min_age"));
+                return film;
+            }
+            System.out.println("Uspesne nahrane z databazy");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return film;
+    }
+    public Film getRecordNormal(){
         Film film = null;
         String sql = "SELECT id,Film_name,Director_name,release_year,actors FROM NormalneFilmy";
         try{
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
             while(rs.next()){
-                //System.out.println(rs.getInt("id") + "\t" + rs.getString("Film_name") + "\t" + rs.getString("Director_name"));
                 String film_name = rs.getString("film_name");
-                String director = rs.getString("Director_name");
-                int year = rs.getInt("release_year");
                 String herci = rs.getString("actors");
                 StringBuilder builder = new StringBuilder(herci);
-                meno = film_name;
+                this.meno_normal = film_name;
                 builder.deleteCharAt(herci.length() - 1);
                 builder = new StringBuilder(builder);
                 builder.deleteCharAt(0);
-                //System.out.println("Meno: " + film_name + " reziser: " + director + " rok vydania: " + year);
                 String[] actorsArr = builder.toString().split(",");
-                //System.out.println("Herci:" + Arrays.toString(actorsArr));
-                film = new Film(film_name, director, year, actorsArr);
-                System.out.println("Uspesne nahrane z databaze");
+                film = new Film(film_name, rs.getString("Director_name"), rs.getInt("release_year"), actorsArr);
                 return film;
             }
+            System.out.println("Uspesne nahrane z databaze");
         }catch(SQLException e){
             e.printStackTrace();
         }
